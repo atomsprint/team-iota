e2afddb20863969b58d8c53149c9a055a90209a9
Hanamichidesu <fuchu.1226@gmail.com>
2026-02-11 14:53:44 +0900

    ここにメッセージを書きます。書かないと上手く行きません。

--- Iota_SDGs/SecondViewController.swift
+++ Iota_SDGs/SecondViewController.swift
@@ -17,7 +17,7 @@ class SecondViewController: UIViewController {
        
     /*
     // MARK: - Navigation hello!なんでもいいです。
-    // In a storyboard-based application, you will often want to do a little preparation before navigation hello!　hello-test-commit
+    // In a storyboard-based application, you will often want to do a little preparation before navigation hello!　hello-test-commit hello!　なんでもいいです。
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
