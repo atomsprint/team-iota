a6962465a2d0d975a634ee3b4b13dd127b4c0213
Hanamichidesu <fuchu.1226@gmail.com>
2026-02-20 19:46:53 +0900

    クイズ（途中）

---
+++ Iota_SDGs/QuizDta.swift
@@ -0,0 +1,8 @@
+//
+//  QuizDta.swift
+//  Iota_SDGs
+//
+//  Created by 永田　花道 on 2026/02/17.
+//
+
+import Foundation
---
+++ Iota_SDGs/ReViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  Iota_SDGs
+//
+//  Created by 永田　花道 on 2026/02/19.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
