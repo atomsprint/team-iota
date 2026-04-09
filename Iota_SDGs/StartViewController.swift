de627cf5d54eaa1541fa52cbb6b69357e6f62bbe
HinataniH <78k.ide.hinata@gmail.com>
2026-04-09 19:01:24 +0900

    starpage

---
+++ Iota_SDGs/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  Iota_SDGs
+//
+//  Created by 井手 on 2026/04/09.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
