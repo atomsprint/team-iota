d53fbecfd4c032a3d1ac4c5d4fd133ca0a2be163
MionaKoba <miona_koba@outlook.jp>
2026-05-02 20:19:48 +0900

    スタート画面を変更しました

--- Iota_SDGs/MainViewController.swift
+++ Iota_SDGs/MainViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var earthImageView: UIImageView!
     
     @IBAction func unwindToHome(segue: UIStoryboardSegue) {}
     
@@ -21,27 +22,42 @@ class MainViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(pointsUpdated), name: .pointsUpdated, object: nil)
     }
     
+    let pointlmageDictionary:[Int:String]=[
+        0:"地球1",
+        10:"地球2",
+        20:"地球2",
+        30:"地球2",
+        40:"地球2",
+        50:"地球2",
+        60:"地球3",
+        70:"地球3",
+        80:"地球3",
+        90:"地球3",
+        100:"地球（ラフ 透明化）"
+    ]
+
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
     
     @objc func pointsUpdated() {
         updatePointsLabel()
+        updateImage()
     }
     
     func updatePointsLabel() {
         let name = SettingsManager.shared.userName.isEmpty ? "ゲスト" : SettingsManager.shared.userName
         pointLabel.text = "\(name)さんは現在\(PointsManager.shared.points)ポイントです"
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func updateImage() {
+        let currentPoint = PointsManager.shared.points
+        // ポイント数に応じて画像を変更（100pt以上は固定）
+        let imageKey = min((currentPoint / 10) * 10, 100)
+        if let imageName = pointlmageDictionary[imageKey] {
+            earthImageView?.image = UIImage(named: imageName)
+        }
     }
-    */
 
 }
