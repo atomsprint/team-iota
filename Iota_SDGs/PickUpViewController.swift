a4ff3136b8a8ab0f26f4ecb2f5c451d045c09a7b
HinataniH <78k.ide.hinata@gmail.com>
2026-05-04 06:04:59 +0900

    取り組みページ,,

---
+++ Iota_SDGs/PickUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PickUpViewController.swift
+//  Iota_SDGs
+//
+//  Created by 井手 on 2026/05/04.
+//
+
+import UIKit
+
+class PickUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
